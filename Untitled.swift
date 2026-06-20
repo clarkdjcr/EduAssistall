debf2162d62c6559fe55ecd3bc7e6193ab8fbcff
clarkdjcr <clarkdjcr@hotmail.com>
2026-06-19 20:56:57 -0400

    Update scratch notes

--- Untitled.swift
+++ Untitled.swift
@@ -5,4 +5,12 @@
 ## The Student needs to be able to send messages back to the teacher with the capability to add attachments to show completion of daily assignments and quizes as applicable.
 ## How about a shared storage space which is sharable with the teacher and also with other students as group assignments may include the requirement for collaboration.  May need to be separate spaces - individual and shared?
 ## The teacher needs to define the grading criteria for the assignments and the student needs to be able to view the criteria.  The haomwork, the quizes, and the group activities and final exam should ve variable percentages of final grade and the teacher needs to be able to adjust the percentage for each.
+/## Review on 6/19/2026 ##/
+/##/ The creation of the lesson plan allows for selection of the grade and the subject.  When the subject is any thing other than math the cooresponding search through external sources should be relativet to the subject matter chosen along with appropriate grade level filteing. Presently this only shows items related to math reguardless of what subject is chosen. Same isse for the grade level condition/##/
+                                                    
+/##/The output of the final lesson plan has an option for review  Once approved, there should be a message sent and a daily asignment that is ready for distribution to either all students or indiviual students.  Presently this function doesn't exist.  The UI indicates that this is available but the actual delivery of the daily assignment is not available for the student./##/
+                                                            
+                                                            
+                                                    
+                                                    
                                                             
